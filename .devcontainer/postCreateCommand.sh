9a186bd2a0716b22e24913376ac2df65238d88a3
sanjaykoul <sanjaykoulshopping@gmail.com>
2025-09-04 19:33:48 +0530

    Update postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+# Create conda environment named 'venv'
+conda create -n venv python=3.10 -y
+
+# Activate the environment and install packages
+source activate venv
 pip install --upgrade pip
 pip install -r requirements.txt
